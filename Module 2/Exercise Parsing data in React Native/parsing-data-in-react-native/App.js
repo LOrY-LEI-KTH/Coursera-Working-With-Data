ce5af520c1c6a94d4d8e76744cfda0e5e9b2c933
Yuchen Lei <yuchenl@kth.se>
2024-08-26 15:40:57 +0200

    Finsish Exercise Parsing data in React Native

--- Module 2/Exercise Parsing data in React Native/parsing-data-in-react-native/App.js
+++ Module 2/Exercise Parsing data in React Native/parsing-data-in-react-native/App.js
@@ -1,9 +1,24 @@
-import { FlatList, Text, SafeAreaView, View, StyleSheet } from 'react-native';
+import { FlatList, Text, SafeAreaView, View, StyleSheet } from "react-native";
+import menuItems from "./menuItems.json";
 
 export default App = () => {
+  const { menu } = menuItems; // destructing
+
+  const renderItem = ({ item }) => (
+    <View style={menuStyles.innerContainer}>
+      <Text style={menuStyles.itemText}>{item.title}</Text>
+      <Text style={menuStyles.itemText}>{"$" + item.price}</Text>
+    </View>
+  );
+
   return (
     <SafeAreaView style={menuStyles.container}>
       <Text style={menuStyles.headerText}>Little Lemon Menu</Text>
+      <FlatList
+        data={menu}
+        keyExtractor={({ id }) => id}
+        renderItem={renderItem}
+      />
     </SafeAreaView>
   );
 };
@@ -12,9 +27,20 @@ const menuStyles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  innerContainer: {
+    paddingHorizontal: 40,
+    paddingVertical: 20,
+    backgroundColor: "#495E57",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  itemText: {
+    color: "#F4CE14",
+    fontSize: 22,
+  },
   headerText: {
-    color: '#F4CE14',
+    color: "#F4CE14",
     fontSize: 30,
-    textAlign: 'center',
+    textAlign: "center",
   },
 });
